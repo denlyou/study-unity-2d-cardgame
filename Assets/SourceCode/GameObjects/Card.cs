413608d42a633288e94b168580c0880d3d5393d1
SBJang <denlyou85@gmail.com>
2020-01-19 09:46:51 +0900

    up

--- Assets/SourceCode/GameObjects/Card.cs
+++ Assets/SourceCode/GameObjects/Card.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class Card : MonoBehaviour
 {
     public string debugName; // [dummy] 구분용
+    public MainSource mainRef; // (주의) 순환참조
     public SpriteRenderer shapeSRref;
     public SpriteRenderer backgroundSRref;
 
+    public bool isMyCard; // 내 카드인가?
+    public int cardIndex; // 카드덱에서 index
     public bool isUsed = false; // 사용한 카드인가?
     public int att; // 공격력
     public int dep; // 방어력
@@ -38,12 +41,66 @@ public class Card : MonoBehaviour
     // @Lifecycle Override
     void Update()
     {
-        /*
-        if(Input.GetMouseButtonDown(0))
-        {
-            Debug.Log(debugName + " 클릭됨");
+        // 
+    }
+
+    private void OnMouseDown()
+    {
+        if( !mainRef.isMyTrun ) {
+            mainRef.SetHintMessage("내 턴이 아닙니다!");
+            return;
         }
-        */
+        // 내 카드인가?
+        if( !isMyCard ) 
+        { // 아니면 return;
+            mainRef.SetHintMessage("나의 카드 중에서 선택해야 합니다!");
+            return;
+        }
+        // 이미 사용항 카드인지?
+        if (this.isUsed)
+        {
+            mainRef.SetHintMessage("이미 사용한 카드입니다!");
+            return;
+        }
+        else
+        {
+            this.transform.Translate(0, 1f, 0);
+            this.isUsed = true;
+            mainRef.SelectMyCard(cardIndex);
+        }
+    }
+
+
+    public void SelectEnemy()
+    {
+        if ( mainRef.isMyTrun || isMyCard )
+        {
+            mainRef.SetHintMessage("!? 로직상 나오면 안됨");
+            return;
+        }
+
+        this.transform.Translate(0, -1f, 0);
+        this.isUsed = true;
+
+        // 낼카드가 없는지 체크
+        List<int> avableMyCardList = new List<int>(); // 사용가능한 카드 리스트
+        for (int i = 0; i < mainRef.myCardList.Length; i++)
+        {
+            if (mainRef.myCardList[i].isUsed) continue; // 사용한 카드면 건너뜀
+            if (mainRef.myCardList[i].bgType == this.bgType // 배경 또는
+                || mainRef.myCardList[i].shape == this.shape) // 모양이 같으면
+            {
+                avableMyCardList.Add(i); // 리스트에 추가
+            }
+        }
+        // 낼카드가 없다면 lose
+        if (avableMyCardList.Count == 0)
+        {
+            mainRef.SetHintMessage("낼수 있는 카드가 없습니다. You Lose");
+            return;
+        }
+        // 턴 변경
+        mainRef.isMyTrun = true;
     }
 
 }
--- Assets/SourceCode/MainSource.cs
+++ Assets/SourceCode/MainSource.cs
@@ -1,18 +1,23 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class MainSource : MonoBehaviour
 {
+
+    public int gameRound = 1; // 게임 라운드
+    public bool isMyTrun = true; // 내 턴인가?
     public int myHP = 3000; // 내 HP
     public int enemyHP = 3000; // 적 HP
 
     // UI 
     public Button btnGameStart; // 게임 시작 버튼
+    public Text hintMessage; // 적 HP 텍스트
     public Text myHPText; // 내 HP 텍스트
     public Text enemyHPText; // 적 HP 텍스트
+    public GameObject myCharator, enemyCharactor;
 
     public Card sampleCard; // 복재용 카드 프리팹
     public Card[] myCardList = new Card[5]; // 내 카드 5개 정보
@@ -21,24 +26,33 @@ public class MainSource : MonoBehaviour
 
     public void GameStart()
     {
-        btnGameStart.enabled = true;
         // TODO 기존의 카드가 있다면 destory // 이건 reset 일까?
-        myHP = 3000; enemyHP = 3000; // HP 초기화
+        btnGameStart.gameObject.SetActive(false); // 게임 시작 버튼 숨김
+        gameRound = 1;
+        isMyTrun = true;
+
+        // 캐릭터 정보 표시
+        myCharator.SetActive(true);
+        enemyCharactor.SetActive(true);
+        // HP 초기화
+        myHP = 3000; enemyHP = 3000; 
         myHPText.text = "HP: " + myHP;
         enemyHPText.text = "HP: " + enemyHP;
+        myHPText.gameObject.SetActive(true);
+        enemyHPText.gameObject.SetActive(true);
 
+        // 카드 설정
         myCardList = SetNewCardList(true); // 내 카드 5개를 설정
         enemyCardList = SetNewCardList(false); // 적 카드 5개를 설정
+
+        // 메세지 설정
+        hintMessage.gameObject.SetActive(true);
+        hintMessage.text = "당신의 턴입니다. 카드를 선택하세요.";
+
     }
     public void GameOver()
     {
         btnGameStart.enabled = true;
-    }
-
-    // @Lifecycle Override
-    private void Update()
-    {
-        
     }
 
     /// <summary> 새 카드 생성 메소드 </summary>
@@ -49,22 +63,55 @@ public class MainSource : MonoBehaviour
         {
             Vector3 position = new Vector3(
                 -6.0f + i*3.0f, // x축: -6 지점부터 3씩 증가하면서 배치
-                (isMe ? -4.0f : 4.0f), //y축: 내꺼면 아래, 적꺼면 위
+                (isMe ? -5.0f : 5.0f), //y축: 내꺼면 아래, 적꺼면 위
             0/*z축*/); 
             newCardList[i] = Instantiate<Card>(sampleCard, position, Quaternion.identity); // 카드 복제
             // https://docs.unity3d.com/2018.4/Documentation/ScriptReference/Object.Instantiate.html
             newCardList[i].debugName = (isMe ? "내" : "적") + "카드 " + (i+1);
+            newCardList[i].mainRef = this;
+            newCardList[i].isMyCard = isMe;
+            newCardList[i].cardIndex = i;
         }
         return newCardList;
     }
-}
 
-/* [TODO]AssetBundle
-public class Resources {
-    // Sprite sprite = Resources.
-    public Resources()
+    // 게임 진행중 안내 메세지 설정
+    public void SetHintMessage(string msg)
     {
-        
+        hintMessage.text = msg;
     }
+
+    // 내가 선택한 카드
+    public void SelectMyCard(int cardIndex)
+    {
+        isMyTrun = false;
+        this.SetHintMessage("상대방의 턴입니다.");
+        // 내가 선택한 카드와 가진 카드에서 낼수 있는 카드만 구하기
+        int selectedCardBgType = myCardList[cardIndex].bgType; // 선택한 카드 색상
+        int selectedCardShape = myCardList[cardIndex].shape; // 선택한 카드 모양
+        
+        List<int> avableEnemyCardList = new List<int>(); // 사용가능한 카드 리스트
+        for (int i=0; i<enemyCardList.Length; i++)
+        {
+            if (enemyCardList[i].isUsed) continue; // 사용한 카드면 건너뜀
+            if ( enemyCardList[i].bgType == selectedCardBgType // 배경 또는
+                || enemyCardList[i].shape == selectedCardShape ) // 모양이 같으면
+            {
+                avableEnemyCardList.Add(i); // 리스트에 추가
+            }
+        }
+        int[] avableEnemyCardArray = avableEnemyCardList.ToArray(); // 배열로 변환
+        // 낼카드가 없다면 Win
+        if (avableEnemyCardArray.Length < 1)
+        {
+            SetHintMessage("적이 낼수 있는 카드가 없습니다. You Win");
+            return;
+        }
+
+        int selectEnemyCardIndex = avableEnemyCardArray[Random.Range(0, avableEnemyCardArray.Length)];
+        Debug.Log(selectEnemyCardIndex);
+        // TODO 잠시 지연후 
+        enemyCardList[selectEnemyCardIndex].SelectEnemy();
+    }
+
 }
-*/
